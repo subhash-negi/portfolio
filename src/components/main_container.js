c7bc89700da2c358a939af8befced85b207aa544
subhash-negi <81283313+subhash-negi@users.noreply.github.com>
2023-01-30 19:05:22 +0530

    second commit

--- src/components/main_container.js
+++ src/components/main_container.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import meriphoto from './subhash.jpeg';
 function main(){
 return(
     <section>
-        <img className='border-4 border-purple-500 ml-16 mt-4 object-cover h-32 w-32 rounded-full' src='https://thumbs.dreamstime.com/b/portrait-handsome-man-beard-mustache-close-up-image-serious-brutal-bearded-dark-background-black-white-photography-172859143.jpg'/>
+        <img className='border-4 border-purple-500 ml-24 mt-4 object-cover h-32 w-32 rounded-full' src={meriphoto}/>
         <p className='mt-3 ml-10 text-left text-lg text-blue-600'>Hi, this is Subhash Negi</p>
         <p className='ml-10 text-left text-lg text-blue-600'>I am a full-stack web developer</p>
         <p className='ml-10 text-left text-lg text-blue-600'>I also develop blockchain based DAPPS</p>
